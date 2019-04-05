ba1c4ea700e38a0e8ad6d1258c798a4efd7aa723
mayanksharma18 <mmayank.sharma18@gmail.com>
2019-04-06 02:04:35 +0530

    initial

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function cal(){
+
+    var a=document.getElementById('Inv').value
+    var b=document.getElementById('Ret').value
+    var c=document.getElementById('dur').value
+
+    mat= a+a*b/100*c
+ return mat;
+}
