909e8a85828e53123f51081889515ec33015afe6
mayanksharma18 <mmayank.sharma18@gmail.com>
2019-04-06 11:27:35 +0530

    new js code

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-
-
 function cal(){
 
-    var a=document.getElementById('Inv').value
-    var b=document.getElementById('Ret').value
-    var c=document.getElementById('dur').value
-
-    mat= a+a*b/100*c
- return mat;
-}
+    var a=parseFloat(document.getElementById('Inv').value)
+    var b=parseFloat(document.getElementById('Ret').value)
+    var c=parseFloat(document.getElementById('dur').value)
+    
+    mat= a*b/100*c+a
+    interest=a*b/100*c
+    document.getElementById('tt').innerHTML=`<b> ${mat} INR</b>`
+    document.getElementById('ii').innerHTML=`<b> ${interest} INR</b>`
+    
+    }
